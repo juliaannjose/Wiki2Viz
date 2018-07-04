5ca68372461ef47d26234098f5dc0b583ef1de6b
Julia Ann Jose <Julia.ann.1997@hotmail.com>
2018-07-04 11:42:10 +0300

    Update wiki_compute.py

--- wiki_compute.py
+++ wiki_compute.py
@@ -5,17 +5,25 @@ import codecs
 import re
 import os
 import argparse
-import time
-import lda
 import numpy as np
 import io
+from sklearn.manifold import TSNE
+import bokeh.plotting as bp
+from bokeh.plotting import save
+from bokeh.models import HoverTool
+from bokeh.models import ColumnDataSource, LabelSet, Label, CustomJS,OpenURL, TapTool
+from bokeh.plotting import figure, output_file, show
+from sklearn.decomposition import PCA
+from sklearn import manifold
 
+
+output_file("sentence_embeddings_fr.html")
 
 def is_ascii(s):
     return all(ord(c) < 128 for c in s)
 
 
-tree = ET.parse('data/enwiki.xml')
+tree = ET.parse('frwiki.xml')          #arwiki.xml for arabic
 root = tree.getroot()
 
 articles_and_titles = []
@@ -25,20 +33,8 @@ for i,page in enumerate(root.findall('{http://www.mediawiki.org/xml/export-0.10/
         if p.tag == "{http://www.mediawiki.org/xml/export-0.10/}title":
             article_title = p.text 
             if not article_title == None: 
-                article_title = re.sub(r"\!|\"|\#|\$|\%|\&|\'|\(|\)|\*|\+|\,|\-|\/|\:|\;|\<|\=|\>|\?|\@|\[|\\|\]|\^|\_|\`|\{|\||\}|\~"," ",article_title)
-                article_title = article_title.replace(u'\xa0', u' ')
-                article_title = article_title.replace('Category',u'')
-                article_title = article_title.replace('Wikipedia',u'')
-                article_title = article_title.replace('{{',u'')
-                article_title = article_title.replace('}}',u'')
-                article_title = article_title.replace('File',u'')
-                article_title = article_title.replace('Image',u'')
-                article_title = article_title.replace('Infobox',u'')
-                article_title = article_title.replace('Templates',u'')
-                article_title = article_title.replace('infobox',u'')
-                article_title = article_title.replace('templates',u'')
-                article_title = article_title.replace('Template',u'')
-                article_title = article_title.replace('template',u'')
+                article_title = re.sub(r"\"|\#|\~"," ",article_title)
+            print article_title
         if p.tag == "{http://www.mediawiki.org/xml/export-0.10/}revision":
             for x in p:
                 if x.tag == "{http://www.mediawiki.org/xml/export-0.10/}text":                    
@@ -46,8 +42,8 @@ for i,page in enumerate(root.findall('{http://www.mediawiki.org/xml/export-0.10/
                     if not article_txt == None:                                                
                         article_txt = article_txt[ : article_txt.find("==")]
                         article_txt = re.sub(r"{{.*}}","",article_txt)
-                        article_txt = re.sub(r"\[\[File:.*\]\]","",article_txt)
-                        article_txt = re.sub(r"\[\[Image:.*\]\]","",article_txt)
+                        article_txt = re.sub(r"\[\[Fichier:.*\]\]","",article_txt)    #article_txt = re.sub(r"\[\[ملف:.*\]\]","",article_txt)
+                        article_txt = re.sub(r"\[\[Média:.*\]\]","",article_txt)      #article_txt = re.sub(r"\[\[ميديا:.*\]\]","",article_txt)
                         article_txt = re.sub(r"\n: \'\'.*","",article_txt)
                         article_txt = re.sub(r"\n!.*","",article_txt)
                         article_txt = re.sub(r"^:\'\'.*","",article_txt)
@@ -55,7 +51,7 @@ for i,page in enumerate(root.findall('{http://www.mediawiki.org/xml/export-0.10/
                         article_txt = re.sub(r"http\S+","",article_txt)
                         article_txt = re.sub(r"\d+","",article_txt)   
                         article_txt = re.sub(r"\(.*\)","",article_txt)
-                        article_txt = re.sub(r"Category:.*","",article_txt)
+                        article_txt = re.sub(r"Catégorie:.*","",article_txt)         #article_txt = re.sub(r"التصنيف:.*","",article_txt) and article_txt = re.sub(r"ويكيبيديا:.*","",article_txt)
                         article_txt = re.sub(r"\| .*","",article_txt)
                         article_txt = re.sub(r"\n\|.*","",article_txt)
                         article_txt = re.sub(r"\n \|.*","",article_txt)
@@ -74,17 +70,13 @@ for i,page in enumerate(root.findall('{http://www.mediawiki.org/xml/export-0.10/
                         article_txt = re.sub(r"\n","",article_txt)  
                         article_txt = re.sub(r"\!|\"|\#|\$|\%|\&|\'|\(|\)|\*|\+|\,|\-|\.|\/|\:|\;|\<|\=|\>|\?|\@|\[|\\|\]|\^|\_|\`|\{|\||\}|\~"," ",article_txt)
                         article_txt = re.sub(r" +"," ",article_txt)
-                        article_txt = article_txt.replace(u'\xa0', u' ')
                         title_and_text = ' '.join([article_title,article_txt])
-                        if not article_txt == None and not article_txt == "" and len(article_txt) > 100 and is_ascii(article_txt) and not "Articles for deletion" in article_title and not "WikiProject" in article_title:
+                        if not article_txt == None and not article_txt == "" and len(article_txt) > 100:
                             articles_and_titles.append(title_and_text)      #list having titles and article text
-                            titles.append(article_title) 
-                            print article_title
+                            titles.append(article_title)
 
-print len(articles_and_titles) 
-print len(titles)                     
-l = articles_and_titles[25001:30000]                   
-t = titles[25001:30000] 
+l = articles_and_titles[:10000]                 
+t = titles[:10000] 
 T = []
 L = []
 for t0,l0 in zip(t,l):            #no duplicate titles (hence articles) created
@@ -94,13 +86,14 @@ for t0,l0 in zip(t,l):            #no duplicate titles (hence articles) created
 
 print len(T)
 print len(L)
-
+print "Computing vectors:"
+              
 #computing average of word vectors obtained with fasttext
 nmax = 500000
 vectors = []
 word2id = {}
 words = []
-with io.open('data/wiki.multi.en.vec', 'r', encoding='utf-8', newline='\n', errors='ignore') as f:
+with io.open('wiki.multi.fr.vec', 'r', encoding='utf-8', newline='\n', errors='ignore') as f:        #wiki.multi.ar.vec for arabic
     next(f)
     for i, line in enumerate(f):
         word, vect = line.rstrip().split(' ', 1)
@@ -115,32 +108,34 @@ X = []
 X_title = []
 i = 0
 for entry,titles in zip(L,T):
-    word_list = entry.strip().split(' ')
+    TITLE = titles
+    titles = titles.replace('(','')              #remove "()","-",":" and convert to lowercase
+    titles= titles.replace(')','')
+    titles= titles.replace('-',' ')
+    titles= titles.replace(':',' ')
+    word_list = titles.strip().split(' ')
     avg_vect = 0
     flag = 0
     for w in word_list:
         for w1,v1 in zip(words,vectors):
-            if w1 == w: 
+            if w1 == w.lower(): 
                 avg_vect = avg_vect + v1
                 flag = 1
                 break
     avg = avg_vect/len(word_list)          #avg for one article (entry)
     i = i+1
-    print i 
-    print avg
+    #print i 
+    #print avg
+    #print TITLE
     if flag!=0:             #if avg not computed for an article (i.e =0) then ignore article and title
         X.append(avg)
-        X_title.append(titles)
+        X_title.append(TITLE)
 
-print len(L)
-print len(X)
-print len(T)
-print len(X_title)
-
+print "writing to file..."
 
 #sentence vectors to file
-X_stack = np.vstack(X)               
-with open('results/english.vec','a') as f:
+X_stack = np.vstack(X)               #vstack for output to file 
+with open('fr_titles.vec','a') as f:       #ar_titles.vec
     for t1,x1 in zip(X_title,X_stack):
         f.write(t1.encode("utf-8"))
         f.write('~')
@@ -149,11 +144,28 @@ with open('results/english.vec','a') as f:
             f.write(' ')
         f.write('\n')
 
+print "performing tsne reduction..."
+#dimensionality reduction using tsne
+tSNE = manifold.TSNE(n_components = 2)
+tSNE_data = tSNE.fit_transform(X)
+
+
+#plotting tSNE_data using Bokeh 
+x = [tSNE_data[i][0] for i in range(len(tSNE_data))]
+y = [tSNE_data[j][1] for j in range(len(tSNE_data))]
+
+s1 = ColumnDataSource(data = dict(x=x,y=y,names=X_title))
+p1= figure(plot_width = 1300, plot_height = 1000, tools="tap", title="Click to view")
+p1.circle('x','y',source = s1, alpha = 0.6,size = 7)
+labels1 = LabelSet(x='x', y='y', text='names', level='glyph',
+                  x_offset=5, y_offset=5, source=s1, render_mode='canvas')
 
 
 
-                 
+url = "https://fr.wikipedia.org/wiki/@names"          #https://ar.wikipedia.org/wiki/@names
+taptool = p1.select(type=TapTool)
+taptool.callback = OpenURL(url=url)
 
-       
-    
 
+p1.add_layout(labels1)
+show(p1)
